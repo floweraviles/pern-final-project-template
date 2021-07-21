a4b479f33401cb86af30a1c7e64480b4a2ff19c8
Obaid Rustemi <orustemi@gmail.com>
2021-07-20 21:16:15 -0400

    add put route

--- back-end/controllers/games.js
+++ back-end/controllers/games.js
@@ -27,7 +27,16 @@ games.get("/", async (req, res) => {
 
 
 
-
+// UPDATE GAME
+games.put("/:id", async (req, res) => {
+  try {
+const {id} = req.params;
+const updatedGame = await updateGame(id, req.body)
+res.status(200).json({success: true, payload: updatedGame})
+  } catch (err){
+    console.log(err)
+  }
+})
 
 
 
