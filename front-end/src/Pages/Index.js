cb6ff8d790d5af640398c1e6a12253a6921a6c8c
Jordan Bobadilla <jordanboba08@gmail.com>
2021-07-21 20:59:50 -0400

    add Index page

---
+++ front-end/src/Pages/Index.js
@@ -0,0 +1,12 @@
+import GamesList from "../Components/GamesList"
+
+const Index = () => {
+    return (
+        <section>
+            <h1>All Our Games</h1>
+            <GamesList/>
+        </section>
+    )
+}
+
+export default Index
