9f0ecdbe4f3cb60f85c06f7138322a465ffd105a
Daliz Cruz <dalizcruz@gmail.com>
2021-07-21 19:57:24 -0500

    game new form done

--- front-end/src/Components/GameNewForm.js
+++ front-end/src/Components/GameNewForm.js
@@ -1,37 +1,55 @@
 import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useHistory, withRouter } from "react-router-dom";
-
 import { apiURL } from "../util/apiURL.js";
+
 const API = apiURL();
 
-
 function GameNewForm() {
-    let history = useHistory()
+  let history = useHistory();
 
-const addGame = async (newGame) => {
-    try {
-        await axios.post(`${API}/songs`, newGame);
-        history.push(`/songs`);
-    } catch (error) {
-        console.log(error)
-    }
-};
-
-const [song, setSong] = useState({
+  const [game, setGame] = useState({
     name: "",
     console: "",
     price: "",
     release_date: "",
     favorites: false,
-    box_image:""
-})
+    box_image: "",
+  });
 
- return (
+  const addGame = async (newGame) => {
+    try {
+      await axios.post(`${API}/games`, newGame);
+      history.push(`/games`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleTextInput = (event) => {
+    setGame({ ...game, [event.target.id]: event.target.value });
+  };
+
+  const handleCheckBox = (event) => {
+    setGame({ ...game, [event.target.id]: !game.favorites });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addGame(game);
+  };
+
+  return (
     <div>
       <form>
-      <label htmlFor="name">Game Name:</label>
-        <input id="name" value={game.name} type="text" onChange={handleTextInput} required />
+        <label htmlFor="name">Game Name:</label>
+        <input
+          id="name"
+          value={game.name}
+          type="text"
+          onChange={handleTextInput}
+          required
+        />
 
         <label htmlFor="console">Console:</label>
         <input id="console" type="text" onChange={handleTextInput} required />
@@ -39,8 +57,13 @@ const [song, setSong] = useState({
         <label htmlFor="price">Price:</label>
         <input id="price" type="text" onChange={handleTextInput} required />
 
-        <label htmlFor="release_date">Release Date:</label>
-        <input id="release_date" type="text" onChange={handleTextInput} required />
+        <label htmlFor="release_date">Release Year:</label>
+        <input
+          id="release_date"
+          type="text"
+          onChange={handleTextInput}
+          required
+        />
 
         <label htmlFor="favorites">Favorites:</label>
         <input id="favorites" type="checkbox" onChange={handleCheckBox} />
@@ -49,7 +72,5 @@ const [song, setSong] = useState({
       </form>
     </div>
   );
-};
-export default withRouter(GameNewForm)
- 
-
+}
+export default withRouter(GameNewForm);
