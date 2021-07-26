4b6ab056a87a790dc87c3485cfeaf1af2641150a
Flower Aviles <itsfloweraviles@gmail.com>
2021-07-25 21:45:48 -0400

    changes to the queries

--- back-end/queries/reviews.js
+++ back-end/queries/reviews.js
@@ -3,8 +3,8 @@ const db = require("../db/dbConfig");
 const getAllReviewsForGame = async (gameId) => {
   try {
     const allReviewsForGame = await db.any(
-      `SELECT * FROM reviews WHERE id = $1`
-      ,gameId
+      `SELECT * FROM reviews WHERE id = $1`,
+      gameId
     );
     return { success: true, payload: allReviewsForGame };
   } catch (err) {
@@ -15,64 +15,72 @@ const getAllReviewsForGame = async (gameId) => {
 
 const getReview = async (id) => {
   try {
-      const review = await db.one("SELECT * FROM reviews WHERE id = $1", id);
-      return { success: true, payload: review };
+    const review = await db.one("SELECT * FROM reviews WHERE id = $1", id);
+    return { success: true, payload: review };
   } catch (e) {
-      console.log(e);
-      return { success: false, payload: e };
+    console.log(e);
+    return { success: false, payload: e };
   }
 };
 
 const newReviewForGame = async (review, gameId) => {
   const { reviewer, title, content, rating } = review;
   try {
-      const created = await db.one(`
+    const created = await db.one(
+      `
           INSERT INTO reviews (reviewer, title, content, rating, game_id) 
           VALUES ($1, $2, $3, $4, $5)
           RETURNING *
-      `, [reviewer, title, content, rating, gameId]);
+      `,
+      [reviewer, title, content, rating, gameId]
+    );
 
-      return { success: true, payload: created };
+    return { success: true, payload: created };
   } catch (e) {
-      console.log(e);
-      return { success: false, payload: e };
+    console.log(e);
+    return { success: false, payload: e };
   }
 };
 
 const updateReview = async (id, review) => {
   const { reviewer, title, content, rating, game_id } = review;
   try {
-      const res = await db.one(`
+    const res = await db.one(
+      `
           UPDATE reviews 
           SET reviewer = $1, title = $2, content = $3, rating = $4, game_id = $5
           WHERE id = $6
           RETURNING * 
-      `, [reviewer, title, content, rating, game_id, id]);
-      return { success: true, payload: res };
+      `,
+      [reviewer, title, content, rating, game_id, id]
+    );
+    return { success: true, payload: res };
   } catch (e) {
-      return { success: false, payload: e };
+    return { success: false, payload: e };
   }
 };
 
 const deleteReview = async (id) => {
   try {
-      const deletedReview = await db.one(`
+    const deletedReview = await db.one(
+      `
           DELETE FROM reviews
           WHERE id = $1
           RETURNING * 
-      `, id);
-      return { success: true, payload: deletedReview };
+      `,
+      id
+    );
+    return { success: true, payload: deletedReview };
   } catch (e) {
-      console.log(e);
-      return { success: false, payload: e };
-  } 
+    console.log(e);
+    return { success: false, payload: e };
+  }
 };
 
 module.exports = {
-    getAllReviewsForGame,
-    getReview,
-    newReviewForGame,
-    updateReview,
-    deleteReview,
-
-}
+  getAllReviewsForGame,
+  getReview,
+  newReviewForGame,
+  updateReview,
+  deleteReview,
+};
