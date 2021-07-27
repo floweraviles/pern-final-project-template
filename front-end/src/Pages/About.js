b7d99dd3698d7222da049eef14a9ba6353975200
Flower Aviles <itsfloweraviles@gmail.com>
2021-07-26 22:35:45 -0400

    added styling

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -14,7 +14,7 @@ const About= () => {
             <h2>Jordan Baradilla</h2>
             <h3>Director of Design</h3>
             <img src=
-            "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F4b9465ae-aac0-48b9-94a5-2fffd7cb958c%2FJordan_Bobadilla_Rosario.jpg?table=block&id=ede5100c-f3e0-4c58-95f3-3429fe97ab5c&spaceId=e2735e9b-8fe9-4c16-bdc1-be142cd1014e&width=2160&userId=89eeb8a5-7e4e-4dbc-bbba-1ea8a7a6dda6&cache=v2" 
+            "https://avatars.githubusercontent.com/u/74875301?v=4" 
             alt="Pic of Jordan" />
 
             <br />
@@ -22,7 +22,7 @@ const About= () => {
             <h2>Dalíz Cruz</h2>
             <h3>Lead Assurance Engineer</h3>
             <img src=
-            "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fc3efbe99-0313-4934-83df-14cb1cceb031%2FDaliz_Cruz.png?table=block&id=8c201d03-10df-4435-8583-1d8e4751c325&spaceId=e2735e9b-8fe9-4c16-bdc1-be142cd1014e&width=1090&userId=89eeb8a5-7e4e-4dbc-bbba-1ea8a7a6dda6&cache=v2" 
+            "https://avatars.githubusercontent.com/u/33786946?v=4" 
             alt="pic of Dalíz" />
 
             <br />
@@ -30,7 +30,7 @@ const About= () => {
             <h2>Obaid Rustemi</h2>
             <h3>Lead Developer</h3>
             <img src=
-            "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe1e80b4a-d8d8-4939-9d8c-354b2317410d%2FObaid_or_Obie_Rustemi.jpg?table=block&id=12e4c1f6-7690-4fd7-b65e-4e86c444c625&spaceId=e2735e9b-8fe9-4c16-bdc1-be142cd1014e&width=1200&userId=89eeb8a5-7e4e-4dbc-bbba-1ea8a7a6dda6&cache=v2" 
+            "https://avatars.githubusercontent.com/u/75175536?v=4" 
             alt="Pic of Obaid" />
 
             <br />
@@ -38,7 +38,7 @@ const About= () => {
             <h2>Flower Aviles</h2>
             <h3>Product Owner</h3>
             <img src=
-            "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F5b8ad8c7-b1ed-4f5a-adc4-fe0572e2d099%2FFlower_Aviles.jpg?table=block&id=e934c57a-029e-44f8-b861-72a2da4e9003&spaceId=e2735e9b-8fe9-4c16-bdc1-be142cd1014e&width=4630&userId=89eeb8a5-7e4e-4dbc-bbba-1ea8a7a6dda6&cache=v2" 
+            "https://avatars.githubusercontent.com/u/75289454?v=4" 
             alt="Pic of Flower" />
 
       </div>
