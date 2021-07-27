f06eb038b23200f9e60967fb644c88ec7f2c4755
Flower Aviles <itsfloweraviles@gmail.com>
2021-07-26 22:38:21 -0400

    finalizing about page

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -10,6 +10,9 @@ const About= () => {
             The collaboration started out of a desire  to create a trustworthy portal for selling and buying authentic and rare retro video games. 
             Our knowledge of the video game industry combined with our deep supply chain relationships and software engineering prowess, helped us design a robust user friendly website with features that include: 
             popular gamers’ favorites and authenticated reviews for buyers and easy game upload for sellers.</h2>
+            <br />
+            <br />
+            <br />
 
             <h2>Jordan Baradilla</h2>
             <h3>Director of Design</h3>
@@ -17,6 +20,7 @@ const About= () => {
             "https://avatars.githubusercontent.com/u/74875301?v=4" 
             alt="Pic of Jordan" />
 
+            <br />
             <br />
 
             <h2>Dalíz Cruz</h2>
@@ -26,7 +30,7 @@ const About= () => {
             alt="pic of Dalíz" />
 
             <br />
-
+            <br />
             <h2>Obaid Rustemi</h2>
             <h3>Lead Developer</h3>
             <img src=
@@ -34,7 +38,8 @@ const About= () => {
             alt="Pic of Obaid" />
 
             <br />
-
+            <br />
+            
             <h2>Flower Aviles</h2>
             <h3>Product Owner</h3>
             <img src=
